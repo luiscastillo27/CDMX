e5bd07cc155b1dcdd111f9d2108a9bc8a3146405
luiscastillo27 <luisyosemite@gmail.com>
2017-11-24 01:51:56 -0600

    inicializacion

---
+++ platforms/ios/www/ctrls/InicioCtrl.js
@@ -0,0 +1,10 @@
+(function(){
+    
+    var app = angular.module('InicioCtrl', []);
+
+    app.controller('InicioCtrl', ['$scope', '$state', '$ionicPopup' ,function ($scope, $state, $ionicPopup) {
+      
+
+    }]);
+
+}());  
